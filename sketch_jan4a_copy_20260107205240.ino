4538bb3ae21619093ab6064d646b8ecf73bedf5b
Ankit Maurya <ankitmauryaemb@gmail.com>
2026-01-07 20:56:24 +0530

    Add files via upload

---
+++ sketch_jan4a_copy_20260107205240.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();  // Celsius
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\tTemperature: ");
+  Serial.print(t);
+  Serial.println(" °C");
+}
